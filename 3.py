6fd9b7a07bd79722b8d1af4be590f709f429676a
0knot <petrikaanton@gmail.com>
2025-10-05 13:55:41 +0200

    s

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 limit=int(input("zadaj horný limit:"))
 if limit<5:
-    print("limit musí biť väčší ako 1")
+    print("limit musí biť väčší ako 5")
 else:
     for i in range(5,limit+1):
         if (i//2)!=(i/2):
